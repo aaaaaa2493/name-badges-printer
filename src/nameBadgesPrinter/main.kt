ce4539233201d7c98b1bd5b8e770a7ff71666b5c
aaaaaa2493 <aaaaaa2493@yandex.ru>
2018-10-07 16:42:40 +0300

    Stage 2 completed

--- src/nameBadgesPrinter/main.kt
+++ src/nameBadgesPrinter/main.kt
@@ -1,7 +1,20 @@
 package nameBadgesPrinter
 
+import java.util.*
+
 fun main(args: Array<String>) {
-    println(" _____________ ")
-    println("| Hyper Skill |")
-    println(" ¯¯¯¯¯¯¯¯¯¯¯¯¯ ")
+
+    val scanner = Scanner(System.`in`)
+
+    print("Enter name and surname: ")
+
+    val name = scanner.next()
+    val surname = scanner.next()
+
+    val badgeMain = "* $name $surname *"
+    val badgeBorder = "*".repeat(badgeMain.length)
+
+    println(badgeBorder)
+    println(badgeMain)
+    println(badgeBorder)
 }
